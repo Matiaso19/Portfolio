44dd2931ac2350bcf77297d14d13266efcd01e2c
Matias Olivera <matiasscrog@gmail.com>
2024-08-11 14:17:06 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+
+import { Urbanist } from "next/font/google";
+
+import "./globals.css";
+
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+
+import Navbar from "@/components/navbar";
+import Header from "@/components/header";
+
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Matias Olivera Portfolio",
+  description: "Portfolio made by Matias Olivera",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>
+        <Navbar />
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
